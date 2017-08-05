88366278892e730ecfb7ece06441316326b18c1f
Diego Freire <diegofreire@powersyst.com.br>
2017-08-05 08:53:27 -0300

    add config example

---
+++ configs.example.php
@@ -0,0 +1,16 @@
+<?php
+
+    // PowerZAP
+    const API_TOKEN = '';
+    const WEBHOOK_TOKEN = '';
+
+    // Watson
+    const WATSON_WORKSPACE = '';
+    const WATSON_USERNAME = '';
+    const WATSON_PASSWORD = '';
+
+    // DB
+    const DB_USER   = 'root';
+    const DB_PASS   = 'root';
+    const DB_HOST   = 'localhost';
+    const DB_SCHEMA = 'pizza_test';
